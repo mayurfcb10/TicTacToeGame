5949c80afd56cd8f2a88812883244dbc8736931c
Mayur <mayurzope555@gmail.com>
2020-10-07 10:52:09 +0530

    [Mayur]Refactored: Displayed the board the board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -14,7 +14,6 @@ public class TicTacToeGame {
 
 	// get input from the user
 	public static char getInput(Scanner input) {
-		
 		char givenInput = input.next().charAt(0);
 		if (givenInput == 'X' || givenInput == 'x')
 			return givenInput;
@@ -25,9 +24,11 @@ public class TicTacToeGame {
 
 	// display the board
 	public static void displayBoard(char[] board) {
-		for (int position = 0; position < board.length; position++) {
-			System.out.print(board[position] + " ");
-		}
+			System.out.println(board[1]+" | "+board[2]+" |"+" "+board[3]);
+			System.out.println("---------");
+			System.out.println(board[4]+" | "+board[5]+" |"+" "+board[6]);
+			System.out.println("---------");
+			System.out.println(board[7]+" | "+board[8]+" |"+" "+board[9]);	
 	}
 
 	public static void main(String[] args) {
