969cb29fb0524754f71f0613021b02bc1cd28532
Mayur <mayurzope555@gmail.com>
2020-10-07 09:04:12 +0530

    [Mayur]Added: Game board in the tic tac toe game class

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -2,8 +2,19 @@ package com.bridgelabz.tictactoe;
 
 public class TicTacToeGame {
 
+	public void createboard(int size) {
+		char[] board = new char[size];
+		for(int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game Program");
+		TicTacToeGame ticTacToe = new TicTacToeGame();
+		ticTacToe.createboard(10);
+
 	}
 
 }
