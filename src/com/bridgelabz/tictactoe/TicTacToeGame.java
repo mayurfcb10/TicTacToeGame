4038ac22f196929e8668dfa826eae76a3f871b76
Mayur <mayurzope555@gmail.com>
2020-10-07 10:38:56 +0530

    [Mayur]Added: Display the board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	// created the board of given size
-	public static void createBoard() {
+	public static char[] createBoard() {
 		char[] board = new char[10];
 		for (int position = 1; position < 10; position++) {
 			board[position] = ' ';
 		}
+		return board;
 	}
 
-	public static char getInput() {
-		Scanner input = new Scanner(System.in);
+	// get input from the user
+	public static char getInput(Scanner input) {
+		
 		char givenInput = input.next().charAt(0);
-		if (givenInput == 'X' || givenInput == 'x') {
+		if (givenInput == 'X' || givenInput == 'x')
 			return givenInput;
-		} else if (givenInput == 'O' || givenInput == 'o') {
+		else if (givenInput == 'O' || givenInput == 'o')
 			return givenInput;
+		return givenInput;
+	}
+
+	// display the board
+	public static void displayBoard(char[] board) {
+		for (int position = 0; position < board.length; position++) {
+			System.out.println(board[position] + " ");
 		}
-		return 'N';
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game Program");
-		createBoard();
-		getInput();
+		Scanner input = new Scanner(System.in);
+		char[] board = createBoard();
+		char chosenValue = getInput(input);
+		displayBoard(board);
 	}
 }
